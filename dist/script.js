1f704b3db4d444737c52a193a50142e146821e56
Dmitry Korochkin <abramslost@gmail.com>
2022-11-29 22:57:00 +0300

    add fix

--- dist/script.js
+++ dist/script.js
@@ -15579,37 +15579,37 @@ var modals = function modals() {
         modal = document.querySelector(modalSelector),
         close = document.querySelector(closeSelector);
     trigger.forEach(function (item) {
-      item.addEventListener('click', function (e) {
+      item.addEventListener("click", function (e) {
         if (e.target) {
           e.preventDefault();
         }
 
         modal.style.display = "block";
-        document.body.style.overflow = "hidden"; // document.body.classList.add('modal-open');
+        document.body.style.overflow = "hidden";
       });
     });
-    close.addEventListener('click', function () {
+    close.addEventListener("click", function () {
       modal.style.display = "none";
-      document.body.style.overflow = ""; // document.body.classList.remove('modal-open');
+      document.body.style.overflow = "";
     });
-    modal.addEventListener('click', function (e) {
+    modal.addEventListener("click", function (e) {
       if (e.target === modal) {
         modal.style.display = "none";
-        document.body.style.overflow = ""; // document.body.classList.remove('modal-open');
+        document.body.style.overflow = "";
       }
     });
   }
 
   function showModalByTime(selector, time) {
     setTimeout(function () {
-      document.querySelector(selector).style.display = 'block';
+      document.querySelector(selector).style.display = "block";
       document.body.style.overflow = "hidden";
     }, time);
   }
 
-  bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
-  bindModal('.phone_link', '.popup', '.popup_close');
-  showModalByTime('.popup', 73000);
+  bindModal(".popup_engineer_btn", ".popup_engineer", ".popup_engineer .popup_close");
+  bindModal(".phone_link", ".popup", ".popup_close");
+  showModalByTime(".popup", 73000);
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (modals);
