6ac0f0a993fe65c51b3ff24a4a184006316b155c
Dmitry Korochkin <abramslost@gmail.com>
2022-12-04 14:30:57 +0300

    Added tab switching functionality#2

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -6,4 +6,5 @@ import tabs from "./modules/tabs";
 window.addEventListener('DOMContentLoaded', () :void => {
     modals();
     tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
+    tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
 });
