2893bd0186c20395b39fcf0418622f393b757e42
Dmitry Korochkin <abramslost@gmail.com>
2022-12-04 17:16:57 +0300

    add forms.ts

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,7 +1,10 @@
 import "./slider";
 import modals from "./modules/modals";
+import forms from "./modules/forms";
 
 
 window.addEventListener('DOMContentLoaded', () :void => {
     modals();
+    forms();
+
 });
