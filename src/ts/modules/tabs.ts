b77f9ba8bc6cc3f022d24ab6847da5d27a083ef0
Dmitry Korochkin <abramslost@gmail.com>
2022-12-06 20:27:13 +0300

    Object destructuring

--- src/ts/modules/tabs.ts
+++ src/ts/modules/tabs.ts
@@ -1,39 +1,47 @@
-export const tabs = (arg: {
-    headerSelector: string,
-    tabSelector: string,
-    contentSelector: string,
-    activeClass: string
-  }): void => {
-    const header: Element = document.querySelector(arg.headerSelector);
-    const tabs: NodeListOf<HTMLElement> = document.querySelectorAll(arg.tabSelector);
-    const contents: NodeListOf<HTMLElement> = document.querySelectorAll(arg.contentSelector);
+export const tabs =
+    ({ 
+        headerSelector,
+        tabSelector, 
+        contentSelector, 
+        activeClass 
+    }: 
+    {
+        headerSelector: string,
+        tabSelector: string,
+        contentSelector: string,
+        activeClass: string
+    }): void => {
 
-    const hideTabContent = (): void => {
-        contents.forEach(content => {
-            content.style.display = 'none';
-        });
-        tabs.forEach(tab => {
-            tab.classList.remove(arg.activeClass)
-        })
-    }
-    const showTabContent = (i: number): void => {
-        contents[i].style.display = 'block'
-        tabs[i].classList.add(arg.activeClass);
-    }
-    hideTabContent();
-    showTabContent(0);
+        const header: Element = document.querySelector(headerSelector)
+        const tabs: NodeListOf<HTMLElement> = document.querySelectorAll(tabSelector);
+        const contents: NodeListOf<HTMLElement> = document.querySelectorAll(contentSelector);
 
-    header.addEventListener('click', (e: Event): void => {
-        const target: Element = e.target as HTMLElement;
-        if (target.classList.contains(arg.tabSelector.replace(/\./, ""))
-            || target.parentElement.classList.contains(arg.tabSelector.replace(/\./, ""))) {
-            tabs.forEach((tab, i): void => {
-                if (target == tab || target.parentNode == tab) {
-                    hideTabContent();
-                    showTabContent(i);
-                }
+        const hideTabContent = (): void => {
+            contents.forEach(content => {
+                content.style.display = 'none';
+            });
+            tabs.forEach(tab => {
+                tab.classList.remove(activeClass)
             })
         }
-    });
-};
+        const showTabContent = (i: number): void => {
+            contents[i].style.display = 'block'
+            tabs[i].classList.add(activeClass);
+        }
+        hideTabContent();
+        showTabContent(0);
+
+        header.addEventListener('click', (e: Event): void => {
+            const target: Element = e.target as HTMLElement;
+            if (target.classList.contains(tabSelector.replace(/\./, ""))
+                || target.parentElement.classList.contains(tabSelector.replace(/\./, ""))) {
+                tabs.forEach((tab, i): void => {
+                    if (target == tab || target.parentNode == tab) {
+                        hideTabContent();
+                        showTabContent(i);
+                    }
+                })
+            }
+        });
+    };
 
