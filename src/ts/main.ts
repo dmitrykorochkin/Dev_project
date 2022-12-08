7f6cd877a38668817196d9457e207eebdb9979fa
Dmitry Korochkin <abramslost@gmail.com>
2022-12-08 21:56:46 +0300

    fix

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,19 +1,11 @@
 import "./slider";
-<<<<<<< HEAD
-import modals from "./modules/modals";
 import forms from "./modules/forms";
-=======
 import { modals, tabs } from "./modules";
->>>>>>> 3a7861d44fe665dc0569a849bbe86c4fd22b6cb7
 
 
 window.addEventListener('DOMContentLoaded', (): void => {
 
     modals();
-<<<<<<< HEAD
-    forms();
-
-=======
 
     tabs({
         headerSelector: '.glazing_slider',
@@ -28,5 +20,5 @@ window.addEventListener('DOMContentLoaded', (): void => {
         contentSelector: '.decoration_content > div > div',
         activeClass: 'after_click'
     });
->>>>>>> 3a7861d44fe665dc0569a849bbe86c4fd22b6cb7
+    forms()
 });
