502272f2360aff15878f0698298f471d76a6f36d
Dmitry Korochkin <abramslost@gmail.com>
2022-12-14 14:12:23 +0300

    fix

--- src/ts/modules/changeModalState.ts
+++ src/ts/modules/changeModalState.ts
@@ -1,37 +1,26 @@
 import checkNumberInputs from './checkNumberInputs'
 
-const changeModalState = (state : any) : void=> {
-    const windowForms : NodeListOf<HTMLElement> = document.querySelectorAll('.balcon_icons_img');
-    const windowWidth : NodeListOf<HTMLElement> = document.querySelectorAll('#width');
-    const windowHeight : NodeListOf<HTMLElement> = document.querySelectorAll('#heigth');
-    const windowType : NodeListOf<HTMLElement> = document.querySelectorAll('#view_type');
-    const windowProfile : NodeListOf<HTMLElement> = document.querySelectorAll('.checkbox');
+const changeModalState = (state: any): void => {
+    const windowForms: NodeListOf<HTMLElement> = document.querySelectorAll('.balcon_icons_img');
+    const windowWidth: NodeListOf<HTMLElement> = document.querySelectorAll('#width');
+    const windowHeight: NodeListOf<HTMLElement> = document.querySelectorAll('#heigth');
+    const windowType: NodeListOf<HTMLElement> = document.querySelectorAll('#view_type');
+    const windowProfile: NodeListOf<HTMLElement> = document.querySelectorAll('.checkbox');
 
     checkNumberInputs('#width')
     checkNumberInputs('#heigth')
 
 
-    const bindActionToElems = (e, elem, prop) : void => {
-        elem.forEach((item, i): void => {
+    const bindActionToElems = (e, elem, prop): void => {
+        elem.forEach((item): void => {
             item.addEventListener(e, () => {
-                switch(item.nodeName) {
-                    case 'SPAN' :
-                        state[prop] = i;
-                        break;
+                switch (item.nodeName) {
+
                     case 'INPUT':
-                        if(item.getAttribute('type') === 'checkbox'){
-                            state[prop] = i === 0 ? 'Холодное' : 'Теплое';
-                            elem.forEach((box, j) => {
-                                box.checked = false;
-                                if(i == j) {
-                                    box.checked = true;
-                                }
-                            })
-                        } else {
-                            state[prop] = item.value;
-                        }
+                        state[prop] = item.value;
+
                         break
-                    case 'SELECT' :
+                    case 'SELECT':
                         state[prop] = item.value;
                         break
                 }
@@ -41,10 +30,10 @@ const changeModalState = (state : any) : void=> {
     bindActionToElems('click', windowForms, 'form');
     bindActionToElems('input', windowHeight, 'height');
     bindActionToElems('input', windowWidth, 'width');
-    bindActionToElems('change', windowType, 'type');
+    bindActionToElems('change', windowType, 'value');
     bindActionToElems('change', windowProfile, 'profile');
 
-    
+
 }
 
 export default changeModalState
